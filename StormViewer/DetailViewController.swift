ca6d7da10a8914e3def1427d2941b5fbd261042d
My Nguyen <My@mys-macbook-pro.local>
2016-08-06 15:57:21 -0700

    finished chapter 3

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var detailImageView: UIImageView!
 
-
-    var detailItem: AnyObject? {
+    var detailItem: String? {
         didSet {
             // Update the view.
             self.configureView()
@@ -22,9 +22,9 @@ class DetailViewController: UIViewController {
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        if let detail = detailItem {
+            if let imageView = detailImageView {
+                imageView.image = UIImage(named: detail)
             }
         }
     }
@@ -32,14 +32,50 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+        configureView()
+        // rightBarButtonItem: button that appears on the right of the navigation bar of the view controller
+        // barButtonSystemItem: .Action displays an arrow coming out of a box
+        // action: call the shareTapped() method when you're tapped
+        // target: the method (shareTapped()) belongs to the current view controller (self)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: #selector(shareSocialTapped))
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func shareTapped() {
+        // create a UIActivityViewController in order to share content with other apps and services
+        // activityItems: an array of items to share
+        // applicationActivities: an array of services to offer
+        let viewController = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        // where the UIActivityViewController should be anchored (appear from)
+        // on iPhone, the UIActivityViewController takes up the full screen
+        // on iPad, it is anchored as a popover to the right bar button item (the share button)
+        viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        // present the UIActivityViewController
+        presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    func shareSocialTapped() {
+        let serviceType = SLServiceTypeFacebook
+        // let serviceType = SLServiceTypeTwitter
+        let viewController = SLComposeViewController(forServiceType: serviceType)
+        viewController.setInitialText("Look at this great picture!")
+        viewController.addImage(detailImageView.image!)
+        viewController.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+        presentViewController(viewController, animated: true, completion: nil)
+    }
 }
 
